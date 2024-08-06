4b71771565047c9dde68d6166bcf632200987526
Md Tanweer Alam <tanweeralam1312@gmail.com>
2024-08-06 22:03:22 +0530

    1.2: Course Information, step 2- bug fixed

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,9 +9,9 @@ const Content = (props) => {
   console.log(props);
   return (
     <>
-      <Part part={props.part} exercises={props.exercises} />
-      <Part part={props.part} exercises={props.exercises}  />
-      <Part part={props.part} exercises={props.exercises}  />
+      <Part part={props.part[0]} exercises={props.exercises[0]} />
+      <Part part={props.part[1]} exercises={props.exercises[1]}  />
+      <Part part={props.part[2]} exercises={props.exercises[2]}  />
     </>
   )
 }
@@ -44,9 +44,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
