7c2d928f5cf31cc5f0765c95eea1621af90eedae
Md Tanweer Alam <tanweeralam1312@gmail.com>
2024-08-12 12:48:28 +0530

    1.6 Unicafe step - 2 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
+
 const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
     <>
+      <h1>give feedback</h1>
 
+      <button>good</button>
+      <button>neutral</button>
+      <button>bad</button>
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>average 0.55555556</p>
+      <p>positive 66.7%</p>
     </>
   )
 }
