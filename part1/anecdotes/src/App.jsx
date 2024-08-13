d81631dabc4eaa41076b97cdb6197374cb1f72f2
Md Tanweer Alam <tanweeralam1312@gmail.com>
2024-08-13 23:23:59 +0530

    Part1- anecdote step 3 completed

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,5 +1,22 @@
 import { useState } from 'react'
 
+const Button = ({text, handleClick}) => {
+  return  <button onClick={handleClick}>{text}</button>
+}
+
+const Anecdote = (props) => {
+  return(
+    <>
+      <h1>{props.headline}</h1>
+      <p>
+        {props.anecdote}
+        <br />
+        {'has ' + props.votes + ' votes'}
+      </p>
+    </>
+  )
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -11,12 +28,62 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
+  const [points, setPoints] = useState(new Uint8Array(anecdotes.length))
+
+  // const [vote, setVote] = useState({})
 
   const [selected, setSelected] = useState(0)
 
+  const handleNext = () => {
+    const len = anecdotes.length
+    const random = Math.floor(Math.random() * len)
+    setSelected(random)
+    // console.log(selected)
+  }
+
+  const handleVote = () => {
+    const newPoint = [...points]
+    newPoint[selected] += 1
+    setPoints(newPoint)
+    // console.log(points)
+  }
+
+  const indexOfMax = (arr) => {
+    if (arr.length === 0) return -1
+    else {
+      let max = arr[0]
+      let maxIndex = 0
+
+      for(let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+          maxIndex = i
+          max = arr[i]
+        }
+      }
+      return maxIndex
+    }
+  }
+
+  // const handleVote = () => {
+  //   let newVote = {...vote}
+  //   if (newVote[selected]) {
+  //   newVote[selected] = newVote[selected] + 1
+  //   }
+  //   else {
+  //     newVote[selected] = 1
+  //   }
+
+  //   setVote(newVote)
+  //   console.log(vote)
+  // }
+
+
   return (
     <div>
-      {anecdotes[selected]}
+      <Anecdote headline="Anecdote of the day"  anecdote={anecdotes[selected]} votes={points[selected]} />
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={handleNext} text="next anecdote" />
+      <Anecdote headline="Anecdote with most views" anecdote={anecdotes[indexOfMax(points)]} votes={points[indexOfMax(points)]} />
     </div>
   )
 }
