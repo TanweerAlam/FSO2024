64f0a902315c450f04e51aded452db5aecf30420
Md Tanweer Alam <tanweeralam1312@gmail.com>
2024-08-15 14:27:31 +0530

    part2.4_2.5-completed (courses mapped)

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -37,26 +37,58 @@ import Course from "./Components/Course"
 // }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <>
       {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
+        courses.map(course => <Course course={course} />)
       }
-    ]
-  }
-
-  return <Course course={course} />
-
+    </>
+  )
 }
 
 export default App
