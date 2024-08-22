2378be174d6eadb67b0cbfc717992cb54d7b1a38
Md Tanweer Alam <tanweeralam1312@gmail.com>
2024-08-22 22:01:36 +0530

    part2.11:- completed (axios/promise & useEffect)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,19 +3,28 @@ import Person from "./Components/Person"
 import Filter from "./Components/Filter"
 import PersonForm from "./Components/PersonForm"
 import Persons from "./Components/Persons"
+import axios from "axios"
+import { useEffect } from "react"
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [filterName, setFilterName] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  const fetchPersons = () => {
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response => {
+      setPersons(response.data)
+      console.log("Fetching persons:", response.data)
+    })
+
+  }
+
+  useEffect(fetchPersons, []);
 
   const handleNewName = (event) => setNewName(event.target.value)
   const handleNewNumber = (event) => setNewNumber(event.target.value)
