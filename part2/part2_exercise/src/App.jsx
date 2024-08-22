99a2ddb6239b566fe40e547ca843a32360fe5361
Md Tanweer Alam <tanweeralam1312@gmail.com>
2024-08-22 17:33:37 +0530

    part2- axios/promises & effect-hooks implemented

--- part2/part2_exercise/src/App.jsx
+++ part2/part2_exercise/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import axios from 'axios'
 import Note from "./Components/Note"
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("a new note...")
   const [showAll, setShowAll] = useState(true)
   // console.log("props in App", notes)
+
+  useEffect(() => {
+    console.log("effect")
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log("promise fullfilled")
+        setNotes(response.data)
+      })
+  }, [])
+  console.log("render", notes.length, "notes")
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
